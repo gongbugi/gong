1bbdb4c776f7955b8abebe54fb6427f923f4629d
gongbugi <rrobi1492@gmail.com>
2023-08-06 11:53:25 +0900

    commit

--- del.php
+++ del.php
@@ -8,14 +8,14 @@ $nick = $_SESSION['nick'];
 $query1 = "select * from data where no='$no'";
 $result = mysqli_query($con, $query1);
 $data = mysqli_fetch_array($result);
-
+/*
 if($nick != $data['nick']) {
     echo "<script>
     alert('작성자 본인만 삭제 가능합니다.');
     location.replace('content.php?no=$no');
     </script>";
 }
-else{
+else{*/
     mysqli_query($con, $query);
     $query = "delete from data where no='$no'";
 
